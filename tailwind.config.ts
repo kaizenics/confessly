f10f2daa03367e5932133a4aa08218814d3c16a0
Niko <nicosejohnsoriano@gmail.com>
2024-01-25 02:46:58 +0800

    chore: new font families and primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+          montserrat: ['Montserrat', 'sans-serif'],
+          'montserrat-thin': ['Montserrat', 'sans-serif', '100'],
+          'montserrat-regular': ['Montserrat', 'sans-serif', '400'],
+          'montserrat-bold': ['Montserrat', 'sans-serif', '700'],
+          'montserrat-semibold': ['Montserrat', 'sans-serif', '600'],
+      },
+      colors: {
+        primary: {
+          100: "#F5F5F5",
+        }
       },
     },
   },
