f6a9e4a96c0b65e79af53ae8c0304c6514e2a78c
Niko <nicosejohnsoriano@gmail.com>
2024-01-28 23:32:04 +0800

    feat: button component and update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,14 @@
-import Image from "next/image";
-import { Container } from "@/component/ui/Container";
-import { FcGoogle } from "react-icons/fc";
+"use client";
+
+import { Header } from "@/component/Header";
 import { Messages } from "@/component/Messages";
-import hearts from "@/assets/hearts.png";
-
-
 
 export default function Home() {
+
   return (
     <main>
-      <Container className="container mx-auto flex flex-col">
-        <div className="flex">
-          <div className="ml-12 mt-40">
-            <h1 className="font-montserrat font-semibold text-[100px] bg-gradient-to-r from-red-300 to-red-500 bg-clip-text text-transparent">
-              Confession Wall
-            </h1>
-            <p className="font-montserrat font-regular text-white text-lg">
-              Start sending anonymous messages now and discover the freedom of
-              valentine{" "}
-            </p>
-            <button className="font-montserrat font-semibold text-white text-md border-2 border-slate-600 bg-transparent px-4 py-3 rounded-md mt-7 flex items-center">
-              <FcGoogle className="mr-3" />
-              Sign in with Google
-            </button>
-          </div>
-          <div className="p-28">
-            <div className="w-[400px] h-[400px]">
-              <Image src={hearts} alt="hearts" layout="responsive" />
-            </div>
-          </div>
-        </div>
-      </Container>
-
-     <Messages />
+      <Header/>
+      <Messages />
     </main>
   );
 }
--- src/component/Messages.tsx
+++ src/component/Messages.tsx
@@ -1,36 +1,50 @@
+import { useState } from "react";
 import { Container } from "@/component/ui/Container";
+import { IoMdClose } from "react-icons/io";
 
 const messages = [
-    {
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      date: "2021-10-10",
-    },
-    {
-      text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      date: "2021-10-10",
-    },
-    {
-      text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
-      date: "2021-10-10",
-    },
-    {
-     text: "huh",
-      date: "2021-10-10",
-    },
-    {
-      text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
-      date: "2021-10-10",
-    },
-    {
-      text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over, Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
-      date: "2021-10-10",
-    },
-  ];
+  {
+    text: "regards ko ni allana marie diha hehe",
+    date: "2021-10-10",
+  },
+  {
+    text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    date: "2021-10-10",
+  },
+  {
+    text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
+    date: "2021-10-10",
+  },
+  {
+    text: "huh",
+    date: "2021-10-10",
+  },
+  {
+    text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
+    date: "2021-10-10",
+  },
+  {
+    text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over, Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over",
+    date: "2021-10-10",
+  },
+];
 
 export const Messages = () => {
-    return (
-        <>
-             <Container className="container flex justify-center items-center px-6 xl:px-0">
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMessage, setSelectedMessage] = useState("");
+
+  const handleReadMore = (message: string) => {
+    setSelectedMessage(message);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMessage("");
+    setShowModal(false);
+  };
+  return (
+    <>
+      <Container className="container flex justify-center items-center px-6 xl:px-0">
         <div className="flex flex-col md:block lg:flex-row xl:flex-row justify-between items-center my-10">
           <div className="lg:mx-24 xl:mx-7 grid grid-cols-4 gap-5">
             {messages.map((message, index) => (
@@ -45,9 +59,12 @@ export const Messages = () => {
                 </p>
                 <div className="w-full">
                   <div className="flex border-t-2 justify-between border-slate-600">
-                    <p className="font-montserrat font-semibold text-md text-white text-right py-3 px-5">
+                    <button
+                      className="font-montserrat font-semibold text-md text-red-400 hover:text-red-500 text-right py-3 px-5 cursor-pointer"
+                      onClick={() => handleReadMore(message.text)}
+                    >
                       Read more
-                    </p>
+                    </button>
                     <p className="font-montserrat font-regular text-md text-white text-right py-3 px-5">
                       {message.date}
                     </p>
@@ -58,9 +75,24 @@ export const Messages = () => {
           </div>
         </div>
       </Container>
-        </>
-    );
-};
 
-            
-      
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="w-[700px] bg-slate-800 p-6 rounded-md inset-0">
+            <div className="flex justify-end">
+              <button
+                className="mb-4 px-1 py-1 border-2 border-gray-500 text-white rounded-md"
+                onClick={() => handleCloseModal()}
+              >
+                <IoMdClose className="w-[20px] h-[20px]" />
+              </button>
+            </div>
+            <p className="font-montserrat font-regular text-lg text-white">
+              {selectedMessage}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
