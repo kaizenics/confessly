f22893534e08824e458f27e3881a630735edf808
Niko <nicosejohnsoriano@gmail.com>
2024-01-28 23:30:32 +0800

    feat: firebase configuration and initialization

---
+++ src/app/firebase.tsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf3iuxuWGvtyr_BulwGdy6cNrZpSoMt-o",
+  authDomain: "confey-db516.firebaseapp.com",
+  projectId: "confey-db516",
+  storageBucket: "confey-db516.appspot.com",
+  messagingSenderId: "479932086706",
+  appId: "1:479932086706:web:e627403d554ad411e54cab",
+  measurementId: "G-X157EVCX08"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
