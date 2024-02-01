0481750ce8ca719c67cc649ba187c41401536b4e
Niko <nicosejohnsoriano@gmail.com>
2024-02-02 03:15:46 +0800

    refactor: firestore updateDoc function

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, updateDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +25,6 @@ export const db = getFirestore(app);
 
 // firebase authentication (useContext)
 export const auth = getAuth(app);
+
+// firestore update document function
+export { updateDoc };
