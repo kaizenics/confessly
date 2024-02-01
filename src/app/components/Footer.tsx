7d3224c57f5bf73e3cf4654119bcc1d9a6cfcafd
Niko <nicosejohnsoriano@gmail.com>
2024-02-01 15:49:30 +0800

    refactor: Footer, Header, and Messages components

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,33 +1,42 @@
-
 import { Container } from "~/app/components/ui/Container";
 
-export const Footer = () => {   
-    return (
-        <>
-        <section className="mt-24">
+export const Footer = () => {
+  return (
+    <>
+      <section className="mt-24">
         <Container className="container mx-auto ">
           <div className="w-[100%] h-[1.5px] bg-slate-600">
             <div className="px-2" />
           </div>
           <footer className="mt-3">
             <div className="flex justify-between items-center">
-              <p className="font-montserrat text-md mb-20 items-start text-[#dfdfdfdc] inline-block  whitespace-nowrap">
+              <p className="font-montserrat text-[8px] sm:text-sm md:text-md mb-20 items-start text-[#dfdfdfdc] inline-block  whitespace-nowrap">
                 Niko Soriano © 2024 All Rights Reserved
               </p>
-              <div className="flex font-montserrat text-md items-start text-[#dfdfdfdc] mb-20 whitespace-nowrap">
-             
-               <a href="https://github.com/kaizenics/confessly" target="_blank" rel="noopener noreferrer">
-                <p className="pl-4 font-semibold hover:text-white hover:underline">Contribute</p>
+              <div className="flex font-montserrat text-[8px] sm:text-sm md:text-md items-start text-[#dfdfdfdc] mb-20 whitespace-nowrap">
+                <a
+                  href="https://github.com/kaizenics/confessly"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="font-semibold hover:text-white hover:underline">
+                    Contribute
+                  </p>
                 </a>
-                <a href="https://github.com/kaizenics/confessly/issues/new" target="_blank" rel="noopener noreferrer">
-                <p className="pl-4 font-semibold hover:text-white hover:underline">Report a bug</p>
+                <a
+                  href="https://github.com/kaizenics/confessly/issues/new"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="pl-4 font-semibold hover:text-white hover:underline">
+                    Report a bug
+                  </p>
                 </a>
-               
               </div>
             </div>
           </footer>
         </Container>
       </section>
-        </>
-    );
-}
+    </>
+  );
+};
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -60,8 +60,14 @@ export const Header = () => {
     <div>
       <Container className="container mx-auto">
         <main className="flex justify-center items-center flex-row mt-32 sm:mt-28 sm:mb-14 ">
-        <div className="top-8 -z-10 absolute sm:hidden">
-            <Image src={hearts} alt="hearts" width={90} height={90} layout="fixed" />
+          <div className="top-8 -z-10 absolute sm:hidden">
+            <Image
+              src={hearts}
+              alt="hearts"
+              width={90}
+              height={90}
+              layout="fixed"
+            />
           </div>
           <div className="relative sm:mr-8">
             {!user ? (
@@ -73,31 +79,34 @@ export const Header = () => {
                   Confession Wall
                 </h1>
                 <p className="font-montserrat font-regular text-center sm:text-left text-white text-[4vw] sm:text-[16px] md:text-[18px] lg:text-lg xl:text-lg">
-                  Sign in with your HCDC account to start sending messages anonymously
+                  Sign in with your Google account to start sending messages
+                  anonymously
                 </p>
                 <div className="flex justify-center sm:justify-normal">
-                <button
-                  className="font-montserrat font-semibold text-white text-md text-md border-2 border-slate-600 bg-transparent rounded-md px-4 py-3 mt-7 flex items-center"
-                  onClick={handleSignIn}
-                >
-                  <FcGoogle className="mr-3" />
-                  Sign in with Google
-                </button>
+                  <button
+                    className="font-montserrat font-semibold text-white text-md text-md border-2 border-slate-600 bg-transparent rounded-md px-4 py-3 mt-7 flex items-center"
+                    onClick={handleSignIn}
+                  >
+                    <FcGoogle className="mr-3" />
+                    Sign in with Google
+                  </button>
                 </div>
               </div>
             ) : (
               <div>
-                <p className="font-montserrat text-white text-center sm:text-start text-[4vw] sm:text-md lg:text-2xl">Welcome, {user.displayName}!</p>
+                <p className="font-montserrat text-white text-center sm:text-start text-[4vw] sm:text-sm lg:text-xl">
+                  Welcome, {user.displayName}!
+                </p>
                 <h1 className="font-montserrat font-semibold text-center sm:text-left text-[10vw] sm:text-[5vw] md:text-[5vw] lg:text-[5.5vw] xl:text-[80px] bg-gradient-to-r from-red-300 to-red-500 bg-clip-text text-transparent cursor-pointer">
                   Confession Wall
                 </h1>
                 <div className="flex justify-center sm:justify-normal">
-                <textarea
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  placeholder="Start putting messages here"
-                  className="xl:m-0 border-2 border-slate-600 bg-gray-900 font-montserrat font-regular text-white w-[100%] h-[130px] sm:h-[100px] rounded-md px-4 py-3 focus:outline-none focus:ring-1 focus:ring-red-300 resize-none"
-                />
+                  <textarea
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="Start putting messages here"
+                    className="xl:m-0 border-2 border-slate-600 bg-gray-900 font-montserrat font-regular text-white w-[100%] h-[130px] sm:h-[100px] rounded-md px-4 py-3 focus:outline-none focus:ring-1 focus:ring-red-300 resize-none"
+                  />
                 </div>
                 <div className="flex flex-wrap justify-center sm:justify-normal">
                   <button
@@ -129,10 +138,16 @@ export const Header = () => {
             )}
           </div>
           <div className=" -z-10 hidden sm:flex">
-            <Image src={hearts} alt="hearts" width={370} height={370} layout="fixed" />
+            <Image
+              src={hearts}
+              alt="hearts"
+              width={370}
+              height={370}
+              layout="fixed"
+            />
           </div>
         </main>
-        </Container>
+      </Container>
     </div>
   );
 };
